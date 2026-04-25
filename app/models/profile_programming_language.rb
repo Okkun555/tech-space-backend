3040b54512ff8d1d46e3e7a319b8e18e68de56b8
Okkun555 <y.okuno.btm@gmail.com>
2026-04-25 22:44:32 +0900

    ProfileProgrammingLanguageのバリデーション追加

--- app/models/profile_programming_language.rb
+++ app/models/profile_programming_language.rb
@@ -3,4 +3,7 @@ class ProfileProgrammingLanguage < ApplicationRecord
   belongs_to :programming_language
 
   validates :programming_language_id, uniqueness: { scope: :profile_id }
+  validates :experience_years,
+            numericality: { greater_than_or_equal_to: 0, only_integer: true },
+            allow_nil: true
 end
